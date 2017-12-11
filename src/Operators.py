c6b68f20f46ba61d6ad7020b407906e4032be28a
Carson Scott <carsonjscott14@gmail.com>
2017-12-11 03:41:05 -0500

    added operators

--- src/Operators.py
+++ src/Operators.py
@@ -1,19 +1,5 @@
 from Functions import *
 
-def AND(o, x):
-	x = getinputs(o, x)
-	for i in range(len(x)):
-		if x[i] == 0:
-			return 0
-	return 1
-
-def OR(o, x):
-	x = getinputs(o, x)
-	for i in range(len(x)):
-		if x[i] == 1:
-			return 1
-	return 0
-
 def GET(o, x):
 	return o[x[0]]
 
@@ -29,7 +15,58 @@ def SET(o, x):
 		x = o[x]
 	o[k] = x
 
+def AND(o, x):
+	x = getinputs(o, x)
+	return x[0] and x[1]
+
+def OR(o, x):
+	x = getinputs(o, x)
+	return x[0] or x[1]
+
+def NOT(o, x):
+	x = getinputs(o, x)
+	return not x[0]
+
+def EQUAL(o, x):
+	x = getinputs(o, x)
+	return x[0] == x[1]
+
+def MORE(o, x):
+	x = getinputs(o, x)
+	return x[0] > x[1]
+
+def LESS(o, x):
+	x = getinputs(o, x)
+	return x[0] < x[1]
+
+def MOE(o, x):
+	x = getinputs(o, x)
+	return x[0] >= x[1]
+
+def LOE(o, x):
+	x = getinputs(o, x)
+	return x[0] <= x[1]
+
 def ADD(o, x):
 	x = getinputs(o, x)
-	return sum(x)
+	return x[0] + x[1]
 
+def SUB(o, x):
+	x = getinputs(o, x)
+	return x[0] - x[1]
+
+def MULT(o, x):
+	x = getinputs(o, x)
+	return x[0] * x[1]
+
+def DIV(o, x):
+	x = getinputs(o, x)
+	return x[0] / x[1]
+
+def SQR(o, x):
+	x = getinputs(o, x)
+	return x[0] ** 2
+
+def ABS(o, x):
+	x = getinputs(o, x)
+	return abs(x[0])
