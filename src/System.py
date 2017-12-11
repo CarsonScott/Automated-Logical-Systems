30c09d17340a4e9c7c948d14efe5c7b7447acc59
Carson Scott <carsonjscott14@gmail.com>
2017-12-11 03:00:17 -0500

    added system

--- src/System.py
+++ src/System.py
@@ -2,3 +2,18 @@ from Object import *
 from Functions import *
 from Operators import *
 from Interpreter import *
+
+class System(Interpreter):
+
+	def __init__(self, operators):
+		super().__init__(operators)
+		self['statements'] = []
+
+	def add_statement(self, statement):
+		self['statements'].append(statement)
+
+	def run(self):
+		y = []
+		for i in range(len(self['statements'])):
+			y.append(self.call(self['statements'][i]))
+		return y
